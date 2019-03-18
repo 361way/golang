11d60f6ca883b9eab4e3f43800de13b22b53ed83
root <root@361way.com>
2019-03-18 14:55:22 +0800

    获取远端请求数据，并以httpserver提供出去

--- http/01getrequest.go
+++ http/01getrequest.go
@@ -19,3 +19,39 @@ func main() {
 	body, _ := ioutil.ReadAll(res.Body)
 	fmt.Println(string(body))
 }
+
+
+/* 也可以结合http请求和http服务,如下代码：
+package main
+ 
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+)
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    url := "http://country.io/capital.json"
+    response, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer response.Body.Close()
+ 
+    responseData, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+ 
+    responseString := string(responseData)
+    fmt.Fprint(w, responseString)
+}
+func main() {
+    http.HandleFunc("/", ServeHTTP)
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
+
+*/
+
